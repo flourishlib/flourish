05c5ceb396a7ea878ae5ac515e7ba4c9ff757aef
wbond <will@wbond.net>
2012-09-12 13:13:04 -0400

    Tweaked pull and push scripts

--- pull_submodules.php
+++ pull_submodules.php
@@ -2,14 +2,21 @@
 `git submodule init`;
 `git submodule update`;
 
-`git --work-tree=./build --git-dir=./build/.git checkout master`;
-`git --work-tree=./classes --git-dir=./classes/.git checkout master`;
-`git --work-tree=./discussion --git-dir=./discussion/.git checkout master`;
-`git --work-tree=./docs --git-dir=./docs/.git checkout master`;
-`git --work-tree=./flourishlib.com --git-dir=./flourishlib.com/.git checkout master`;
-`git --work-tree=./phpdoc --git-dir=./phpdoc/.git checkout master`;
-`git --work-tree=./site --git-dir=./site/.git checkout master`;
-`git --work-tree=./tests --git-dir=./tests/.git checkout master`;
-`git --work-tree=./tests-results --git-dir=./tests-results/.git checkout master`;
-`git --work-tree=./wiki-engine --git-dir=./wiki-engine/.git checkout master`;
-
+$submodules = array(
+	'build',
+	'classes',
+	'discussion',
+	'docs',
+	'flourishlib.com',
+	'phpdoc',
+	'site',
+	'tests',
+	'tests-results',
+	'wiki-engine'
+);
+foreach ($submodules as $submodule) {
+	chdir($submodule);
+	`git checkout master`;
+	`git pull origin master`;
+	chdir('..');
+}
--- push_submodules.php
+++ push_submodules.php
@@ -1,14 +1,19 @@
 <?php
-`git add build`;
-`git add classes`;
-`git add discussion`;
-`git add docs`;
-`git add flourishlib.com`;
-`git add phpdoc`;
-`git add site`;
-`git add tests`;
-`git add tests-results`;
-`git add wiki-engine`;
+$submodules = array(
+	'build',
+	'classes',
+	'discussion',
+	'docs',
+	'flourishlib.com',
+	'phpdoc',
+	'site',
+	'tests',
+	'tests-results',
+	'wiki-engine'
+);
+foreach ($submodules as $submodule) {
+	`git add $submodule`;
+}
 
 `git commit -m 'Added new submodule commits'`;
 `git push origin master`;
